79e0c10c97a4c8dbe35d780c0c0078fe6239488c
BabisK <babis_k@outlook.com>
2016-07-19 19:01:13 +0300

    Continue

--- part1/ex4.py
+++ part1/ex4.py
@@ -1,8 +1,25 @@
-from nltk import downloader
+import re
+from nltk import downloader, ngrams, FreqDist
+from nltk.corpus.util import LazyCorpusLoader
+from nltk.corpus.reader import EuroparlCorpusReader
+from nltk.probability import LaplaceProbDist
+from pprint import pprint
 
 def main():
     downloader.download('europarl_raw')
-    from nltk.corpus import europarl_raw
+    english = LazyCorpusLoader('europarl_raw/english', EuroparlCorpusReader, r'ep-.*\.en', encoding='utf-8')
+    tokens = english.words()
+
+    bigrams = [ gram for gram in ngrams(tokens, 2) ]
+    trigrams = [ gram for gram in ngrams(tokens, 3) ]
+
+    counts = FreqDist(bigrams)
+        
+    #laplace = LaplaceProbDist(counts)
+
+    pprint(counts.N())
+
+    #print(len(set(english.words())))
 
 if __name__ == "__main__":
     main()
