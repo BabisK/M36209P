4f2e4fa336291d4e687c2b3de7db0913a80d47d8
Charalampos Kaidos <babis_k@outlook.com>
2016-07-26 02:40:11 +0300

    Started viterbi

--- part3/ex3.py
+++ part3/ex3.py
@@ -8,3 +8,56 @@ from nltk.probability import LaplaceProbDist, ConditionalFreqDist, ConditionalPr
 from nltk.tokenize import word_tokenize
 import matplotlib
 
+def ngrams_sentences(sentences, n):
+    ngrams_serntences = []
+    for sentence in sentences:
+        for i in range(n - 1):
+            sentence = ['start{}'.format(i)] + sentence + ['end{}'.format(i)]
+        ngrams_serntences.append([gram for gram in ngrams(sentence, n)])
+    return ngrams_serntences
+
+def viterbi(sentence, vocabulary, conditional_probability_distribution):
+    V = [{}]
+    for word in vocabulary:
+        V[0][word] = {'prob': conditional_probability_distribution['start0'].prob(word)}
+
+
+    new_sentences, vk1 = viterbi(sentence[:-1], conditional_probability_distribution)
+    v = [conditional_probability_distribution[new_sentence[-1]].max() for new_sentence in new_sentences]
+    return new_sentence, v
+
+def main():
+    matplotlib.use('Qt5Agg')
+    import matplotlib.pyplot as plt
+
+    download('punkt')
+    # Download and load the english europarl corpus
+    downloader.download('europarl_raw')
+    english = LazyCorpusLoader('europarl_raw/english', EuroparlCorpusReader, r'ep-.*\.en', encoding='utf-8')
+
+    words = english.words()
+
+    # Calculate the frequency distribution of the words in the corpus
+    word_frequency_distribution = FreqDist([word.lower() for word in words])
+
+    # Get the sentences of the corpus, all in lower case, with infrequent words replaced by the token "<unknown>"
+    sentences = [[word.lower() if word_frequency_distribution[word.lower()] >= 10 else '<unknown>' for word in sentence]
+                 for sentence in english.sents()]
+
+    # create train and test dataset
+    train = sentences[0:int(len(sentences) * 0.8)]
+    test = sentences[int(len(sentences) * 0.8):]
+
+    vocabulary_length = word_frequency_distribution.B()
+
+    # Calculate bigrams
+    bigrams_train = list(chain.from_iterable(ngrams_sentences(train, 2)))
+
+    # Calculate the conditional frequency distribution for bigrams
+    bigrams_fd = ConditionalFreqDist(((f,), s) for f, s in bigrams_train)
+
+    # Calculate the conditional probability distribution for bigrams
+    cpd_bigram = ConditionalProbDist(bigrams_fd, LaplaceProbDist, vocabulary_length)
+
+if __name__ == "__main__":
+    main()
